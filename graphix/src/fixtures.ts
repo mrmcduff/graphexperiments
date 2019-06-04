e4b1401b40f0dc80c8a00792c0ac703c4f4ccc0f
Michael McDuffee <mmcduffee@lyft.com>
2019-06-04 15:07:01 -0700

    updating fixture data to make it more dense

--- graphix/src/fixtures.ts
+++ graphix/src/fixtures.ts
@@ -1,9 +1,8 @@
 import { ChartData } from "./types";
-import { string } from "prop-types";
 
-const valuesControl: number[] = Array.from({length: 24}, () => +(Math.random() * 10).toFixed(2));
-const valuesTreatment: number[] = Array.from({length: 24}, () => +(Math.random() * 10).toFixed(2));
-const valuesOtherTreatment: number[] = Array.from({length: 24}, () => +(Math.random() * 10).toFixed(2));
+const valuesControl: number[] = Array.from({length: 48}, () => +(Math.random() * 10).toFixed(2));
+const valuesTreatment: number[] = Array.from({length: 48}, () => +(Math.random() * 10).toFixed(2));
+const valuesOtherTreatment: number[] = Array.from({length: 48}, () => +(Math.random() * 10).toFixed(2));
 
 function zip(dates: Date[], values: number[]): ChartData.VariantDataPoint[] {
     return dates.map((aVal, aInd) => {
@@ -39,6 +38,31 @@ const dates: Date[] = [
     new Date("2019-01-01T00:21:00.000Z"),
     new Date("2019-01-01T00:22:00.000Z"),
     new Date("2019-01-01T00:23:00.000Z"),
+    new Date("2019-01-01T00:24:00.000Z"),
+    new Date("2019-01-01T00:25:00.000Z"),
+    new Date("2019-01-01T00:26:00.000Z"),
+    new Date("2019-01-01T00:27:00.000Z"),
+    new Date("2019-01-01T00:28:00.000Z"),
+    new Date("2019-01-01T00:29:00.000Z"),
+    new Date("2019-01-01T00:30:00.000Z"),
+    new Date("2019-01-01T00:31:00.000Z"),
+    new Date("2019-01-01T00:32:00.000Z"),
+    new Date("2019-01-01T00:33:00.000Z"),
+    new Date("2019-01-01T00:34:00.000Z"),
+    new Date("2019-01-01T00:35:00.000Z"),
+    new Date("2019-01-01T00:36:00.000Z"),
+    new Date("2019-01-01T00:37:00.000Z"),
+    new Date("2019-01-01T00:38:00.000Z"),
+    new Date("2019-01-01T00:39:00.000Z"),
+    new Date("2019-01-01T00:40:00.000Z"),
+    new Date("2019-01-01T00:41:00.000Z"),
+    new Date("2019-01-01T00:42:00.000Z"),
+    new Date("2019-01-01T00:43:00.000Z"),
+    new Date("2019-01-01T00:44:00.000Z"),
+    new Date("2019-01-01T00:45:00.000Z"),
+    new Date("2019-01-01T00:46:00.000Z"),
+    new Date("2019-01-01T00:47:00.000Z"),
+
 ];
 
 const controlVariantData: ChartData.VariantData = {
@@ -68,15 +92,15 @@ export const SampleTriData: ChartData.MetricData = {
 };
 
 export const colormap = new Map<string, string>();
-colormap.set('control', '#785EF0');
-colormap.set('treatment', '#0DC98B');
-colormap.set('treatment2', '#FF4C4D');
-export const COLOR_ROSE_3 = '#E281BF';
-export const COLOR_DESERT_3 = '#FF4C4D';
-export const COLOR_ORANGE_3 = '#FF7545';
-export const COLOR_SUNSHINE_3 = '#FFB13B';
-export const COLOR_GREEN_3 = '#0DC98B';
-export const COLOR_AQUA_3 = '#10AEBA';
-export const COLOR_SKY_3 = '#81BEF0';
-export const COLOR_ULTRAMARINE_3 = '#036AFF';
-export const COLOR_VOILET_3 = '#785EF0';
+export const COLOR_ROSE = '#E281BF';
+export const COLOR_DESERT = '#FF4C4D';
+export const COLOR_ORANGE = '#FF7545';
+export const COLOR_SUNSHINE = '#FFB13B';
+export const COLOR_GREEN = '#0DC98B';
+export const COLOR_AQUA = '#10AEBA';
+export const COLOR_SKY = '#81BEF0';
+export const COLOR_ULTRAMARINE = '#036AFF';
+export const COLOR_VIOLET = '#785EF0';
+colormap.set('control', COLOR_VIOLET);
+colormap.set('treatment', COLOR_GREEN);
+colormap.set('treatment2', COLOR_DESERT);
